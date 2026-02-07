270a66f6dcb5e3677e873ab13e12d30861a75229
Nishant Dahiya <dahiya4145@gmail.com>
2026-02-07 16:57:00 +0530

    new updated v11

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("trust proxy", 1);
 // ---------- ALLOWED ORIGINS ----------
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://shopingnis.netlify.app",
+  "https://sheshrifashion.com",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
